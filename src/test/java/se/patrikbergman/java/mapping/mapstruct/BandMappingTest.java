9c6120ec0a6422173d4983f646fda22a7f7cdc67
Patrik Bergman <patrik.bergman@transmode.com>
2016-04-18 14:37:00 +0200

    mapstruct seems a bit unstable - difficult to make it work with immutables - difficult to make it work with lombok - sometimes problems with idea
    Added simple example with java beans style.
    Will wait for progress in mapstruct project

--- src/test/java/se/patrikbergman/java/mapping/mapstruct/BandMappingTest.java
+++ src/test/java/se/patrikbergman/java/mapping/mapstruct/BandMappingTest.java
@@ -1,21 +1,3 @@
-/**
- * Copyright 2012-2013 Gunnar Morling (http://www.gunnarmorling.de/)
- * and/or other contributors as indicated by the @authors tag. See the
- * copyright.txt file in the distribution for a full listing of all
- * contributors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package se.patrikbergman.java.mapping.mapstruct;
 
 
@@ -25,7 +7,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-public class ConversionTest {
+public class BandMappingTest {
 
     Band source;
     BandMapper mapper;
